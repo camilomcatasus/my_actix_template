e24e0b31d5251ff743e8f9b982ee5838d31c0594
Camilo Catasus <camilomcatasus@gmail.com>
2023-08-21 17:16:27 -0400

    Initial commit

---
+++ database_macros/src/lib.rs
@@ -0,0 +1,152 @@
+extern crate proc_macro;
+use quote::{quote, ToTokens};
+use proc_macro::TokenStream;
+use syn::{ parse_macro_input, DeriveInput, Data, Fields, FieldsNamed, Ident};
+
+
+#[proc_macro_derive(Queryable)]
+pub fn print_tokens(input: TokenStream) -> TokenStream {
+    let ast: DeriveInput = parse_macro_input!(input as DeriveInput);
+    let new_functions: proc_macro2::TokenStream;
+    // Check if the input is a struct
+    if let Data::Struct(data_struct) = ast.data {
+        let struct_name = ast.ident;
+
+        match data_struct.fields {
+            Fields::Named(fields_named) => {
+                let get_fn_tokens = body_get(&fields_named, &struct_name);
+                let add_fn_tokens = body_add(&fields_named, &struct_name);
+                let update_fn_tokens = body_update(&fields_named, &struct_name);
+                new_functions = quote! {
+                    impl #struct_name {
+                        #get_fn_tokens
+                        #add_fn_tokens
+                        #update_fn_tokens
+                    }
+                }
+            }
+            _ => panic!("Only structs with named fields are supported"),
+        }
+    } else {
+        panic!("Only structs are supported");
+    }
+    println!("{}", new_functions);
+    return TokenStream::from(new_functions);
+}
+
+fn body_get(fields_named: &FieldsNamed, struct_name: &Ident) -> proc_macro2::TokenStream {
+
+    let struct_name_string = String::from(struct_name.to_string());
+    let idents: Vec<_> = fields_named.named.iter().map(|f| &f.ident).collect();
+    let index: Vec<_> = fields_named.named.iter().enumerate().map(|f| f.0).collect();
+    let types: Vec<_> = fields_named.named.iter().map(|f| &f.ty).collect();
+    let conditions: Vec<String> = idents.iter().map(|ident| format!("AND {} = ", ident.as_ref().unwrap())).collect();
+    quote! {
+        pub fn get(#(#idents : Option<#types>),*) -> anyhow::Result<Self> {
+            let conn: rusqlite::Connection = rusqlite::Connection::open(DATABASE_PATH)?;
+            let mut query_string: String = format!("SELECT * FROM {} WHERE TRUE = TRUE", #struct_name_string);
+            #(
+                if let Some(i) = #idents {
+                    query_string = format!("{}\n{}{}", query_string, #conditions, i);
+                }
+             )*
+            let mut stmt = conn.prepare(&query_string)?;
+            let obj = stmt.query_row([], |row| {
+                Ok(#struct_name {
+                    #(#idents : row.get(#index)?,)*
+                })
+            })?;
+
+            return Ok(obj);
+        }
+
+        pub fn get_many(#(#idents : Option<#types>),*) -> anyhow::Result<Vec<Self>> {
+            let conn: rusqlite::Connection = rusqlite::Connection::open(DATABASE_PATH)?;
+            let mut query_string: String = format!("SELECT * FROM {} WHERE TRUE = TRUE", #struct_name_string);
+            #(
+                if let Some(i) = #idents {
+                    query_string = format!("{}\n{}{}", query_string, #conditions, i);
+                }
+             )*
+
+            let mut stmt = conn.prepare(&query_string)?;
+            let obj_iter = stmt.query_map([], |row| {
+                Ok(#struct_name {
+                    #(#idents : row.get(#index)?,)*
+                })
+            })?;
+
+            let obj_vector = obj_iter.map(|x| x.unwrap()).collect();
+
+            return Ok(obj_vector);
+
+        }
+    }
+}
+
+fn body_add(fields_named: &FieldsNamed, struct_name: &Ident) -> proc_macro2::TokenStream {
+    let struct_name_string = String::from(struct_name.to_string());
+    let idents: Vec<_> = fields_named.named.iter().skip(1).map(|f| &f.ident).collect();
+    let types: Vec<_> = fields_named.named.iter().skip(1).map(|f| &f.ty).collect();
+    let type_strings: Vec<_> = types.into_iter().map(|t| {
+        let mut token_stream = proc_macro2::TokenStream::new();
+        t.to_tokens(&mut token_stream);
+        let t_string: &str = &token_stream.to_string();
+        match t_string {
+            "String" => return String::from("\"{}\""),
+            _ => return String::from("{}")
+        }
+    }).collect();
+    let joined_brackets = type_strings.join(", ");
+    
+    let var_strings: Vec<_> = idents.iter().filter_map(|&opt| opt.as_ref()).map(|ident| ident.to_string()).collect();
+    let joined_vars: String = var_strings.join(", ");
+    let query_string: String = format!("INSERT INTO {} ({}) VALUES ({});", struct_name_string, joined_vars, joined_brackets);
+
+    quote! {
+        pub fn add(&self) -> anyhow::Result<usize> {
+            let conn: rusqlite::Connection = rusqlite::Connection::open(DATABASE_PATH)?;
+            let query_string: String = format!(#query_string #(, self.#idents)* );
+            let stmt: usize = conn.execute(&query_string, ())?;  
+            return Ok(stmt);
+        }
+    }
+}
+
+fn body_update(fields_named: &FieldsNamed, struct_name: &Ident) -> proc_macro2::TokenStream {
+    let struct_name_string = String::from(struct_name.to_string());
+    let idents: Vec<_> = fields_named.named.iter().map(|f| &f.ident).collect();
+    let types: Vec<_> = fields_named.named.iter().map(|f| &f.ty).collect();
+    let type_strings: Vec<_> = types.into_iter().map(|t| {
+        let mut token_stream = proc_macro2::TokenStream::new();
+        t.to_tokens(&mut token_stream);
+        let t_string: &str = &token_stream.to_string();
+        match t_string {
+            "String" => return String::from("\"{}\""),
+            _ => return String::from("{}")
+        }
+    }).collect();
+
+    let first_ident = idents.get(0).unwrap();
+    
+    let var_strings: Vec<_> = idents.iter().filter_map(|&opt| opt.as_ref()).map(|ident| ident.to_string()).collect();
+    let mut up_strings: Vec<String> = Vec::new();
+
+    for index in 1..type_strings.len() {
+        up_strings.push(format!("{} = {}", var_strings.get(index).unwrap(), type_strings.get(index).unwrap()));
+    }
+
+    let joined_up_strings: String = up_strings.join(",\n");
+    let query_string: String = format!("UPDATE {} SET {} WHERE {} = {{}} ;", struct_name_string, joined_up_strings, var_strings.get(0).unwrap());
+    let skipped_idents: Vec<_> = idents.iter().skip(1).map(|f| *f).collect();
+    
+    quote! {
+        pub fn update(&self) -> anyhow::Result<usize> {
+            let conn: rusqlite::Connection = rusqlite::Connection::open(DATABASE_PATH)?;
+            let query_string: String = format!(#query_string #(, self.#skipped_idents)*, self.#first_ident);
+            //
+            let stmt: usize = conn.execute(&query_string, ())?;  
+            return Ok(stmt);
+        }
+    }
+}
---
+++ server/src/main.rs
@@ -0,0 +1,57 @@
+use std::cell::RefCell;
+
+use actix_files as fs;
+use actix_web::http::header::ContentType;
+use actix_web::{web, App, HttpServer, HttpRequest, HttpResponse };
+use minijinja::value::Value;
+use minijinja::{path_loader, Environment};
+
+thread_local! {
+    static CURRENT_REQUEST: RefCell<Option<HttpRequest>> = RefCell::default()
+}
+
+fn with_bound_req<F, R>(req: &HttpRequest, f: F) -> R
+where 
+    F: FnOnce() -> R, 
+{
+    CURRENT_REQUEST.with(|current_req| *current_req.borrow_mut() = Some(req.clone()));
+    let rv = std::panic::catch_unwind(std::panic::AssertUnwindSafe(f));
+    CURRENT_REQUEST.with(|current_req| current_req.borrow_mut().take());
+    match rv {
+        Ok(rv) => rv,
+        Err(panic) => std::panic::resume_unwind(panic),
+    }
+}
+
+pub struct AppState {
+    env: minijinja::Environment<'static>,
+}
+
+impl AppState {
+    pub fn render_template(&self, name: &str, req: &HttpRequest, ctx: Value) -> HttpResponse {
+        with_bound_req(req, || {
+            let tmpl = self.env.get_template(name).unwrap();
+            let rv = tmpl.render(ctx).unwrap();
+            HttpResponse::Ok()
+                .content_type(ContentType::html())
+                .body(rv)
+        })
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    
+    let mut env = Environment::new();
+    env.set_loader(path_loader("pages"));
+    let state = web::Data::new(AppState { env });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(state.clone())
+            .service(fs::Files::new("/static", "./static").show_files_listing())
+   })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
