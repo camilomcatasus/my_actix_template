a94260af600a0a3601be353283f51d459a715fea
Camilo Catasus <camilomcatasus@gmail.com>
2023-11-03 14:12:36 -0400

    Add get macros for libsql_client crate

--- database_macros/src/lib.rs
+++ database_macros/src/lib.rs
@@ -35,6 +35,34 @@ pub fn print_tokens(input: TokenStream) -> TokenStream {
     return TokenStream::from(new_functions);
 }
 
+#[proc_macro_derive(Bindable)]
+pub fn print_binding_tokens(input: TokenStream) -> TokenStream {
+    let ast: DeriveInput = parse_macro_input!(input as DeriveInput);
+    let new_functions: proc_macro2::TokenStream;
+    if let Data::Enum(enum_struct) = ast.data {
+        let enum_name = ast.ident;
+
+        new_functions = quote! {
+            impl rusqlite::types::ToSql for #enum_name {
+                fn to_sql(&self) -> rusqlite::Result<rusqlite::types::ToSqlOutput<'_>> {
+                    Ok(self.to_string().into())
+                }
+            }
+
+            impl rusqlite::types::FromSql for #enum_name {
+                fn column_result(value: rusqlite::types::ValueRef<'_>) -> rusqlite::types::FromSqlResult<Self> {
+                    value.as_str()?.parse()
+                        .map_err(|e| rusqlite::types::FromSqlError::Other(Box::new(e)))
+                }
+            }
+        }
+    } else {
+        panic!("Only enums are supported");
+    }
+    println!("{}", new_functions);
+    return TokenStream::from(new_functions);
+}
+
 fn request_struct(fields_named: &FieldsNamed, struct_name: &Ident) -> proc_macro2::TokenStream {
     let request_struct: &Ident = &Ident::new(&format!("{}Request", struct_name), proc_macro2::Span::call_site());
     let idents: Vec<_> = fields_named.named.iter().map(|f| &f.ident).collect();
@@ -48,13 +76,6 @@ fn request_struct(fields_named: &FieldsNamed, struct_name: &Ident) -> proc_macro
     }
 }
 
-fn get_conn_type(is_pool: bool) -> proc_macro2::TokenStream {
-    match is_pool {
-        false => return quote! { &rusqlite::Connection },
-        true => return quote! { &r2d2::PooledConnection<r2d2_sqlite::SqliteConnectionManager> }
-    }
-}
-
 fn body_get(fields_named: &FieldsNamed, struct_name: &Ident) -> proc_macro2::TokenStream {
     let request_struct: &Ident = &Ident::new(&format!("{}Request", struct_name), proc_macro2::Span::call_site());
     let struct_name_string = String::from(struct_name.to_string());
@@ -62,9 +83,6 @@ fn body_get(fields_named: &FieldsNamed, struct_name: &Ident) -> proc_macro2::Tok
     let index: Vec<_> = fields_named.named.iter().enumerate().map(|f| f.0).collect();
     let types: Vec<_> = fields_named.named.iter().map(|f| &f.ty).collect();
     let conditions: Vec<String> = idents.iter().map(|ident| format!("AND {} = ", ident.as_ref().unwrap())).collect();
-
-
-     
 
     quote! {
         pub fn get(conn: &rusqlite::Connection, request: #request_struct) -> anyhow::Result<Self> {
@@ -165,6 +183,91 @@ fn body_update(fields_named: &FieldsNamed, struct_name: &Ident) -> proc_macro2::
             let query_string: String = format!(#query_string, self.#first_ident);
             let stmt: usize = conn.execute(&query_string, rusqlite::params![#( self.#skipped_idents),*])?;  
             return Ok(stmt);
+        }
+    }
+}
+
+#[proc_macro_derive(LibSqlQueryable)]
+pub fn libsql_macro(input: TokenStream) -> TokenStream {
+    let ast: DeriveInput = parse_macro_input!(input as DeriveInput);
+    let new_functions: proc_macro2::TokenStream;
+    // Check if the input is a struct
+    if let Data::Struct(data_struct) = ast.data {
+        let struct_name = ast.ident;
+
+        match data_struct.fields {
+            Fields::Named(fields_named) => {
+                let request = request_struct(&fields_named, &struct_name);
+                let get_fn_tokens = libsql_body_get(&fields_named, &struct_name);
+                //let add_fn_tokens = body_add(&fields_named, &struct_name);
+                //let update_fn_tokens = body_update(&fields_named, &struct_name);
+                new_functions = quote! {
+                    #request
+
+                    impl #struct_name {
+                        #get_fn_tokens
+                        //#add_fn_tokens
+                        //#update_fn_tokens
+                    }
+                }
+            }
+            _ => panic!("Only structs with named fields are supported"),
+        }
+    } else {
+        panic!("Only structs are supported");
+    }
+    return TokenStream::from(new_functions);
+}
+
+fn libsql_body_get(fields_named: &FieldsNamed, struct_name: &Ident) -> proc_macro2::TokenStream {
+    let request_struct: &Ident = &Ident::new(&format!("{}Request", struct_name), proc_macro2::Span::call_site());
+    let struct_name_string = String::from(struct_name.to_string());
+    let idents: Vec<_> = fields_named.named.iter().map(|f| &f.ident).collect();
+    let types: Vec<_> = fields_named.named.iter().map(|f| &f.ty).collect();
+    let conditions: Vec<String> = idents.iter().map(|ident| format!("AND {} = ", ident.as_ref().unwrap())).collect();
+
+    quote! {
+        pub async fn get(client: &libsql_client::Client, request: #request_struct) -> anyhow::Result<Self> {
+            let mut query_string: String = format!("SELECT * FROM {} WHERE TRUE = TRUE", #struct_name_string);
+            let mut to_sql_objects: Vec<libsql_client::Value> = Vec::new();
+            #(
+                let mut #idents: #types;
+                if let Some(i) = request.#idents {
+                    query_string = format!("{}\n{}?", query_string, #conditions);
+                    #idents = i.clone();
+                    to_sql_objects.push(#idents.into());
+                }
+            )*
+
+            let obj: #struct_name = client.execute(libsql_client::Statement::with_args(&query_string, &to_sql_objects)).await?
+                .rows
+                .iter()
+                .next()
+                .map(libsql_client::de::from_row::<#struct_name>)
+                .transpose()?
+                .context("No rows returned")?;
+            return Ok(obj);
+        }
+        
+        pub async fn get_many(client: &libsql_client::Client, request: #request_struct) -> anyhow::Result<Vec<Self>> {
+            let mut query_string: String = format!("SELECT * FROM {} WHERE TRUE = TRUE", #struct_name_string);
+            let mut to_sql_objects: Vec<libsql_client::Value> = Vec::new();
+            #(
+                let mut #idents: #types;
+                if let Some(i) = request.#idents {
+                    query_string = format!("{}\n{}?", query_string, #conditions);
+                    #idents = i.clone();
+                    to_sql_objects.push(#idents.into());
+                }
+            )*
+
+            let obj_vector = client.execute(libsql_client::Statement::with_args(&query_string, &to_sql_objects)).await?
+                .rows
+                .iter()
+                .map(libsql_client::de::from_row)
+                .collect::<Result<Vec<#struct_name>, _>>()?;
+
+            return Ok(obj_vector);
         }
     }
 }
