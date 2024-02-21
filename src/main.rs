68ac673c0bed1cc4a8c0ef65281afab7386b2af2
Camilo Catasus <camilomcatasus@gmail.com>
2024-02-20 20:57:15 -0500

    Separated template into branch for different projects

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,12 @@
-use std::cell::RefCell;
-
 use actix_files as fs;
 use actix_web::http::header::ContentType;
-use actix_web::{web, App, HttpServer, HttpRequest, HttpResponse };
+use actix_web::{web, App, HttpServer, HttpResponse };
 use minijinja::value::Value;
 use minijinja::{path_loader, Environment};
 use r2d2_sqlite::SqliteConnectionManager;
 
 mod models;
 
-thread_local! {
-    static CURRENT_REQUEST: RefCell<Option<HttpRequest>> = RefCell::default()
-}
-
-fn with_bound_req<F, R>(req: &HttpRequest, f: F) -> R
-where 
-    F: FnOnce() -> R, 
-{
-    CURRENT_REQUEST.with(|current_req| *current_req.borrow_mut() = Some(req.clone()));
-    let rv = std::panic::catch_unwind(std::panic::AssertUnwindSafe(f));
-    CURRENT_REQUEST.with(|current_req| current_req.borrow_mut().take());
-    match rv {
-        Ok(rv) => rv,
-        Err(panic) => std::panic::resume_unwind(panic),
-    }
-}
 
 pub struct AppState {
     env: minijinja::Environment<'static>,
@@ -32,16 +14,15 @@ pub struct AppState {
 }
 
 impl AppState {
-    pub fn render_template(&self, name: &str, req: &HttpRequest, ctx: Value) -> HttpResponse {
-        with_bound_req(req, || {
-            let tmpl = self.env.get_template(name).unwrap();
-            let rv = tmpl.render(ctx).unwrap();
-            HttpResponse::Ok()
-                .content_type(ContentType::html())
-                .body(rv)
-        })
+    pub fn render_template(&self, name: &str, ctx: Value) -> HttpResponse {
+        let tmpl = self.env.get_template(name).unwrap();
+        let rv = tmpl.render(ctx).unwrap();
+        HttpResponse::Ok()
+            .content_type(ContentType::html())
+            .body(rv)
     }
 }
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: 'class',
-	content: ["./**/*.{html,rs}"],
+	content: ["./**/*.{html,rs,js,ts}"],
   theme: {
   },
   plugins: [
